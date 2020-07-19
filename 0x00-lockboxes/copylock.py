6d340f720023e7638eedb2ae84bbf9d6f39f8045
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-07-19 00:23:28 -0500

    missing check 11

---
+++ 0x00-lockboxes/copylock.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """
+        lockboxes function to unlock
+        boxes (list): list of boxes
+    """
+    key = [0]
+    tof = 0
+    for i in range(len(boxes)):
+        for j in boxes[i]:
+            if i != j and j not in key:
+                key.append(j)
+            if i + 1 not in key:
+                break
+    if i not in key:
+    #     tof = 1
+    # if tof != 0:
+        return False
+    else:
+        return True
