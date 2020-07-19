ac6ce1ab2af600203bc39d229354ce67a3653748
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-07-19 00:20:13 -0500

    lockboxes try

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -11,14 +11,16 @@ def canUnlockAll(boxes):
         key = [0]
         tof = 0
         for i in range(len(boxes)):
+            if i not in key:
+                tof = i
             for j in boxes[i]:
                 if i != j and j not in key:
                     key.append(j)
+                if tof in key:
+                    tof = 0
                 if i + 1 not in key:
                     break
         key.sort()
-        if i not in key:
-            tof = 1
     if tof != 0:
         return False
     else:
