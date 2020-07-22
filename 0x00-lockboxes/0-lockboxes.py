eec833889386abd7c18339e87591be8c45dffccf
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-07-21 22:19:04 -0500

    another try to unlock

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,17 +9,23 @@ def canUnlockAll(boxes):
     """
     if boxes:
         key = [0]
-        tof = 0
+        """tof = 0"""
         for i in range(len(boxes)):
             for j in boxes[i]:
-                if i != j and j not in key:
+                if j < len(boxes) and j not in key:
                     key.append(j)
                 if i + 1 not in key:
                     break
+        if len(key) == len(boxes):
+            return True
+        return False
+        """
         key.sort()
         if i not in key:
             tof = 1
-    if tof != 0:
-        return False
-    else:
-        return True
+        if tof != 0:
+            return False
+        else:
+            return True
+    return False
+        """
