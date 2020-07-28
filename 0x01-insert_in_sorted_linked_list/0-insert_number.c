a14e50c098c9b4fdfea0c846390488e5464966b2
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-07-28 00:29:31 -0500

    fixing conditionals

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -26,7 +26,7 @@ listint_t *insert_node(listint_t **head, int number)
 		}
 	else
 	{
-		while (current->next != NULL && current->n < number)
+		while (current->next != NULL && current->next->n < number)
 			current = current->next;
 		new->next = current->next;
 		current->next = new;
