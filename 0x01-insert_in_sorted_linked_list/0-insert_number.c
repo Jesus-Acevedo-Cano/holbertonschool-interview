ab7f87c0360d047aa42e43fb46eb511a66629a3e
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-07-28 00:12:30 -0500

    fixing conditionals

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *temp, *current = *head;
+	listint_t *new, *current = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -20,19 +20,16 @@ listint_t *insert_node(listint_t **head, int number)
 	new->next = NULL;
 
 	if (*head == NULL || (*head)->n >= number)
-		new->next = *head;
+		{
+			new->next = *head;
+			*head = new;
+		}
 	else
 	{
-		while (current->next != NULL)
-		{
-			if (current->next->n >= number && current->n < number)
-			{
-				temp = current;
-				new->next = temp->next;
-				current->next = new;
-			}
+		while (current->next != NULL && current->n < number)
 			current = current->next;
-		}
+		new->next = current->next;
+		current->next = new;
 	}
 	return (new);
 }
