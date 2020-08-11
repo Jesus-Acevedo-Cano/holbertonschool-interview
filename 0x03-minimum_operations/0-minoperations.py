48d64fa2d2c2bef3eee0d079e1e3430230b646be
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-08-10 22:26:01 -0500

    min operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -2,3 +2,35 @@
 """
 Method that calculates the fewest number of operations
 """
+
+
+def minOperations(n):
+
+    count = 2
+    if n == int or n < 1:
+        return 0
+    else:
+        while n > 1:
+            if n % 2 != 0:
+                n = n + 1
+                count += 1
+            if n % 2 == 0:
+                n = n / 2
+                count += 1
+
+    return count
+
+"""
+i = 1
+
+n = 4
+
+for j in range(n):
+    if i <= n/2:
+        i = i + i
+        print (i, j)
+    elif i > n/2 and i <= n - n%2 :
+        i = i + n%2
+        print (i, j)
+
+             """
