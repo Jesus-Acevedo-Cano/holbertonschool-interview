7882f12c8c56712ff550f9de98a04765351bcde1
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-08-18 02:51:05 -0500

    sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -13,5 +13,56 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
+	int i, j, x, y, num;
 
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+			for (x = 0; x < 3; x++)
+			{
+				for (y = 0; y < 3; y++)
+				{
+					num = grid1[x][y];
+					if (num >= 4)
+					{
+						printf("=\n");
+						print_grid(grid1);
+						grid1[x][y] -= 4;
+						if (x + 1 < 3)
+							grid1[x + 1][y]++;
+						if (x - 1 >= 0)
+							grid1[x - 1][y]++;
+						if (y + 1 < 3)
+							grid1[x][y + 1]++;
+						if (y - 1 >= 0)
+							grid1[x][y - 1]++;
+					}
+				}
+			}
+		}
+	}
+}
+
+/**
+ * print_grid - Function that computes the sum of two sandpiles.
+ * @grid: Sandpile
+ *
+ * Return: nothing.
+*/
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
 static void print_grid(int grid[3][3]);
 
 #endif /* SANDPILES */
