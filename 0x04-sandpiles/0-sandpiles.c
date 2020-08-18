ecf8bc0ee43fa084295ccb09ee6c4f52955330d5
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-08-18 03:03:57 -0500

    refactoring

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -13,34 +13,14 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, x, y, num;
+	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
-			for (x = 0; x < 3; x++)
-			{
-				for (y = 0; y < 3; y++)
-				{
-					num = grid1[x][y];
-					if (num >= 4)
-					{
-						printf("=\n");
-						print_grid(grid1);
-						grid1[x][y] -= 4;
-						if (x + 1 < 3)
-							grid1[x + 1][y]++;
-						if (x - 1 >= 0)
-							grid1[x - 1][y]++;
-						if (y + 1 < 3)
-							grid1[x][y + 1]++;
-						if (y - 1 >= 0)
-							grid1[x][y - 1]++;
-					}
-				}
-			}
+			redistribute(grid1);
 		}
 	}
 }
@@ -64,5 +44,38 @@ static void print_grid(int grid[3][3])
 			printf("%d", grid[i][j]);
 		}
 		printf("\n");
+	}
+}
+
+/**
+ * redistribute - Function that distributes the sand grains
+ * @grid1: Sandpile
+ *
+ * Return: nothing.
+*/
+void redistribute(int grid1[3][3])
+{
+	int x, y, num;
+
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			num = grid1[x][y];
+			if (num >= 4)
+			{
+				printf("=\n");
+				print_grid(grid1);
+				grid1[x][y] -= 4;
+				if (x + 1 < 3)
+					grid1[x + 1][y]++;
+				if (x - 1 >= 0)
+					grid1[x - 1][y]++;
+				if (y + 1 < 3)
+					grid1[x][y + 1]++;
+				if (y - 1 >= 0)
+					grid1[x][y - 1]++;
+			}
+		}
 	}
 }
