0e0ad72b49860acbb06c99908e12f96f3f29aacb
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-08-24 19:27:59 -0500

    proof

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,23 +9,4 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current;
-
-	if (head == NULL || *head == NULL)
-		return (1);
-
-	current = *head;
-
-	for (i = 0; current != NULL; i++)
-	{
-		pal_list[i] = current->n;
-		current = current->next;
-	}
-	i -= 1;
-	for (int x = 0; x <= i; x++, i--)
-	{
-		if (pal_list[x] != pal_list[i])
-			return (0);
-	}
-	return (1);
 }
