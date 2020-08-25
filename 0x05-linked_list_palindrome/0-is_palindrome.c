080b750994dfaa74209547103c94544f89b66b79
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-08-24 19:26:09 -0500

    proof

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,14 +1,15 @@
 #include "lists.h"
+
 /**
  * is_palindrome - check if a linked list is a palindrome
  * @head: pointer to the head of the linked list
  *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
+
 int is_palindrome(listint_t **head)
 {
 	listint_t *current;
-	int i;
 
 	if (head == NULL || *head == NULL)
 		return (1);
