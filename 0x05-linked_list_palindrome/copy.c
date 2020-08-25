beca2580e5fb8d0eb4ec8c57be84ecd08c6679af
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-08-24 19:32:38 -0500

    proof

---
+++ 0x05-linked_list_palindrome/copy.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * is_palindrome - check if a linked list is a palindrome
+ * @head: pointer to the head of the linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	int pal_list[50];
+	int i;
+	listint_t *current;
+
+	if (head == NULL || *head == NULL)
+		return (1);
+
+	current = *head;
+
+	for (i = 0; current != NULL; i++)
+	{
+		pal_list[i] = current->n;
+		current = current->next;
+	}
+	i -= 1;
+	for (int x = 0; x <= i; x++, i--)
+	{
+		if (pal_list[x] != pal_list[i])
+			return (0);
+	}
+	return (1);
+}
