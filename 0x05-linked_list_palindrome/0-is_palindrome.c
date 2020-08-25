19e079f572a9e1096e4a952ed04cdd95da7380a2
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-08-24 19:47:51 -0500

    so what now?

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,48 +1,29 @@
 #include "lists.h"
 /**
- * is_palindrome - check is a palindrome
- * @head: pointer to head of list
- * Return: 1: palindrome, 0: not palindrome
+ * is_palindrome - check if a linked list is a palindrome
+ * @head: pointer to the head of the linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *currentS = NULL;
-	listint_t *currentF = NULL;
+	listint_t *current = *head;
+	int pal_list[2048];
+	int i = 0, x = 0;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL || current->next == NULL)
 		return (1);
 
-	currentS = *head;
-	currentF = *head;
-
-	if (is_palindrome_rec(currentS, currentF) == NULL)
-		return (0);
-	return (1);
-}
-/**
- * is_palindrome_rec - check is a palindrome recursion
- * @currentS: pointer to refer the head, the start
- * @currentF: pointer to move from end to start
- * Return: pointer: palindrome, NULL: not palindrome
- */
-listint_t *is_palindrome_rec(listint_t *currentS, listint_t *currentF)
-{
-	listint_t *res = NULL;
-
-	if (currentF != NULL)
+	for (i = 0; current != NULL; i++)
 	{
-		res = is_palindrome_rec(currentS, currentF->next);
-		if (res == NULL)
-			return (NULL);
-
-		if (res->n != currentF->n)
-			return (NULL);
-
-		if (res->next == NULL)
-			return (res);
-
-		return (res->next);
+		pal_list[i] = current->n;
+		current = current->next;
 	}
-
-	return (currentS);
+	i -= 1;
+	for (; x <= i; x++, i--)
+	{
+		if (pal_list[x] != pal_list[i])
+			return (0);
+	}
+	return (1);
 }
