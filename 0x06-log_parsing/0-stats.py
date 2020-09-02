23d46c280859e8f8d46263a4c78606507aa65cad
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-09-02 04:09:54 -0500

    check 9 error, try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,31 +2,31 @@
 import sys
 import fileinput
 
-psc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-    }
-file_size = 0
-counter = 0
-
-
-def print_metrics():
-    """ function to print metrics """
-    try:
-        print('File size: {}'.format(file_size))
-        for key in sorted(psc.keys()):
-            if psc[key]:
-                print("{}: {}".format(key, psc[key]))
-    except:
-        pass
-
 if __name__ == "__main__":
+    psc = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+        }
+    file_size = 0
+    counter = 0
+
+
+    def print_metrics():
+        """ function to print metrics """
+        try:
+            print('File size: {}'.format(file_size))
+            for key in sorted(psc.keys()):
+                if psc[key]:
+                    print("{}: {}".format(key, psc[key]))
+        except:
+            pass
+
     try:
         for line in sys.stdin:
             line = line.strip().split(' ')
