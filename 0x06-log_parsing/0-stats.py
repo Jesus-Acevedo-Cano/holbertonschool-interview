d2dd8dcd629a25970ad3122b973957b4ea47f9e9
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-09-02 04:12:34 -0500

    check 9 error, try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,45 +2,42 @@
 import sys
 import fileinput
 
-if __name__ == "__main__":
-    psc = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-        }
-    file_size = 0
-    counter = 0
+psc = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+    }
+file_size = 0
+counter = 0
 
 
-    def print_metrics():
-        """ function to print metrics """
-        try:
-            print('File size: {}'.format(file_size))
-            for key in sorted(psc.keys()):
-                if psc[key]:
-                    print("{}: {}".format(key, psc[key]))
-        except:
-            pass
+def print_metrics():
+    """ function to print metrics """
 
-    try:
-        for line in sys.stdin:
-            line = line.strip().split(' ')
-            file_size += int(line[8])
-            counter += 1
-            status_code = line[7]
+    print('File size: {}'.format(file_size))
+    for key in sorted(psc.keys()):
+        if psc[key]:
+            print("{}: {}".format(key, psc[key]))
 
-            for key in sorted(psc.keys()):
-                if status_code == key:
-                    psc[key] += 1
-            if counter % 10 == 0:
-                print_metrics()
+try:
+    for line in sys.stdin:
+        line = line.strip().split(' ')
+        file_size += int(line[8])
+        counter += 1
+        status_code = line[7]
 
-    except KeyboardInterrupt:
-        print_metrics()
-        raise
+        for key in sorted(psc.keys()):
+            if status_code == key:
+                psc[key] += 1
+        if counter % 10 == 0:
+            print_metrics()
+
+except KeyboardInterrupt:
     print_metrics()
+    raise
+print_metrics()
