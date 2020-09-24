371168daed2caccaada5fc275eb09ab7e3e89eff
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-09-23 23:59:10 -0500

    proof2

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,9 +5,14 @@ method that determines if a given data set represents a valid UTF-8 encoding
 
 
 def validUTF8(data):
+    validation = 0
     for i in data:
-        try:
-            s = i.decode('utf-8')
-        except:
-            return False
-        return True
+        i = format(i, "#010b")[-8:]
+        if validation != 0:
+            validation -= 1
+        elif i[0] == "1":
+            if validation == 1 or validation > 4:
+                return False
+            validation -= 1
+
+    return True if validation == 0 else False
