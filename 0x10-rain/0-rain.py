c276354d983ef4bb47483079baa3a12e202361c3
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-24 23:53:45 -0500

    init files

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -8,15 +8,15 @@ def rain(walls):
     if len(walls) < 1:
         return 0
     for i in range(len(walls)):
-         # Find the maximum element on its left 
+        # Find the maximum element on its left
         left = walls[i]
         for j in range(i):
             left = max(left, walls[j])
 
-        # Find the maximum element on its right 
+        # Find the maximum element on its right
         right = walls[i]
 
-        for j in range(i + 1 , len(walls)):
+        for j in range(i + 1, len(walls)):
             right = max(right, walls[j])
 
         # Update the maximum water
