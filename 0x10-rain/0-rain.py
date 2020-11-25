590d9a21d8c422e8aef06c89dcd28b3e245933fd
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-11-24 23:33:52 -0500

    init files

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,5 +4,11 @@
 
 def rain(walls):
     """ calculate contained raindrops """
+    raindrops = 0
     if len(walls) < 1:
         return 0
+    for i in range(len(walls)):
+        raindrops += walls[i]
+        if walls[i] == 0:
+            raindrops -= 1
+    return raindrops
