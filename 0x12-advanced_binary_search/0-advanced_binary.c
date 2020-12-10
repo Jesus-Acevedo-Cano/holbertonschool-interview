7c40b372f38a5c212aa1282d24dd2aa5cdf36941
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-12-09 23:13:52 -0500

    -_-'

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+/**
+ * advanced_binary - searches for a value in a
+ * sorted array of integers
+ * @array: array to find
+ * @size: array length
+ * @value: value to find
+ *
+ * Return: index
+ */
+int advanced_binary(int *array, size_t size, int value)
---
+++ 0x12-advanced_binary_search/README.md
@@ -0,0 +1,5 @@
+# 0x12-advanced_binary_search
+
+## content
+
+- ### 0-advanced_binary.c
