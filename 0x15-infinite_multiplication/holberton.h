96a6511449473dd5af10aa3b344f897855a86547
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-01-12 23:26:46 -0500

    init files

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,17 @@
+#ifndef INFINITE_MULTIPLICATION
+#define INFINITE_MULTIPLICATION
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/** PROTOTYPES */
+
+int _putchar(char c);
+
+/** 0. Infinite Multiplication */
+void error(void);
+int check(char *number);
+int main(int argc, char *argv[]);
+
+#endif /* INFINITE_MULTIPLICATION */
